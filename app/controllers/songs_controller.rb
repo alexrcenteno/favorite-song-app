3c0248ad0ba6349aa49ba14146b77ebc84acb2cc
alexrcenteno <alex.centeno1991@gmail.com>
2023-01-20 11:32:42 -0600

    add create section

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,16 @@
 class SongsController < ApplicationController
   def index
-    @songs = Songs.all
+    @songs = Song.all
     render :index
+  end
+
+  def create
+    @song = Song.create(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    render :show
   end
 end
